d3f3bf30ee999aa23600bc8144fdfc52a645a4f6
TranDinhGiaBao0 <baotdgpd07429@fpt.edu.vn>
2023-03-09 08:18:57 +0700

    lab1

---
+++ lab1/app.js
@@ -0,0 +1,15 @@
+// javascript for lab1
+//khai bao bien a va nhap gia tri tu bam phim
+//nhap so nguyen a vao bam phim
+let a = prompt("moi ban nhap so nguyen a:");
+let a1 = parseInt(a);
+//nhap so nguyen b vao bam phim
+let b = prompt("moi ban nhap so nguyen b");
+let b1= parseInt(b);
+//tinh tong cua a va b
+let c = a1 +b1;
+let x = a + b;
+//in ra ket qua dong thoi bang 2 cach
+document.writeln("Gia tri cua c="=(c));
+console.log("Gia tri cua c="=(c));
+
