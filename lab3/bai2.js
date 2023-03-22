fcea776f7b906cfa5b20ba11157f58913c0816f2
TranDinhGiaBao0 <baotdgpd07429@fpt.edu.vn>
2023-03-22 09:37:02 +0700

    lab3

---
+++ lab3/bai2.js
@@ -0,0 +1,13 @@
+var result = document.getElementById("result");
+
+function getResult(num) {
+    result.value += num;
+}
+
+function clearResult() {
+    result.value = "";
+}
+
+function calculate() {
+    result.value = eval(result.value);
+}
